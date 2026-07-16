4918d937bf4680c16c15fb29000d1d8247184ba0
unknown <privalovov4@gmail.com>
2026-07-16 20:57:57 +0300

    Add Order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+
+class Order extends Model
+{
+    use HasFactory , SoftDeletes;
+    protected $fillable = [
+        'client_id',
+        'worker_id',
+        'title',
+        'description',
+        'price',
+        'status',
+        'deadline'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function worker()
+    {
+        return $this->belongsTo(User::class, 'worker_id');
+    }
+}
+
+
